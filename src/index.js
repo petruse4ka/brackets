3e524b22cf9b0afc76f9a993b470428d7c396077
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2024-08-11 01:00:28 +0200

    feat: add solution to the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  let testArr = [];
+  
+  for (let i = 0; i < str.length; i++) {
+
+    for (let j = 0; j < bracketsConfig.length; j++) {
+
+      if (str[i] === bracketsConfig[j][0]) {
+        if (bracketsConfig[j][0] === bracketsConfig[j][1]) {
+          if (testArr[testArr.length - 1] === str[i]) {
+            testArr.pop();
+            } else {
+            testArr.push(str[i]);
+            }
+        } else {
+          testArr.push(str[i]);
+        }
+        break;
+      }
+
+      if (str[i] === bracketsConfig[j][1]) {
+        if (testArr[testArr.length - 1] !== bracketsConfig[j][0]) {
+          return false;
+        } else {
+            testArr.pop();
+        }
+        break;
+      }
+    }
+  }
+
+  if ( testArr.length === 0) {
+    return true;
+  } else { 
+    return false;
+  }
 }
